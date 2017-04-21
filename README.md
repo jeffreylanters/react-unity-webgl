7870874d568c4215f9355dea9c66d776688929cb
Jeffrey Lanters <jeffrey@hulan.nl>
2017-04-21 15:10:55 +0200

    Updated npm scripts

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # react-unity-webgl
 A Unity WebGL component for your React application.
+
+# installation
+Install using npm. Make sure you download the version matching with your Unity version. I try to update this plugin in case of need as fast as possible.
+
+```sh
+# example
+$ npm install --save react-unity-webgl@5.6.0
+```
+
+# usage
+To get stated import the Unity component from 'react-unity-webgl'. Once imported you can use the Unity component to load in your Unity content. Place the Unity tag along with a src to the json file Unity exported.
+
+```js
+import React, { Component } from 'react';
+import { Unity } from '/Projects/react-unity-webgl';
+
+export class App extends Component {
+    render() {
+        return (<div className="app">
+            <Unity src="Build/viewer.json" />
+        </div>)
+    }
+}
+```
+
+# communication
+Unity allows you to send Javascript messages to the Unity content. In order to do so using React you have to import the Message function from 'react-unity-webgl'. The first parameter is the target game object name, the next is the method name, and the last is a optional parameter value.
+
+```js
+import React, { Component } from 'react';
+import { Message } from '/Projects/react-unity-webgl'
+
+export class Menu extends Component {
+    onClick () {
+        Message ("myGameObjectName", "myMethodName", "paramterValue");
+    }
+    render() {
+        return (<div className="menu">
+            <div onClick={this.onClick.bind(this)}>
+                Click me
+            </div>
+        </div>)
+    }
+}
+```
+
+# styling
+The player will be injected in the a component with the className "unity-container". To style to player use the following sass styling. To style the loader you can style the component with the className "unity-loader". See the example below.
+
+```scss
+/* Example styling */
+.unity-container {
+    canvas {
+        /* ... */
+    }
+}
+.unity-loader {
+    /* ... */
+    .bar {
+        /* ... */
+        .fill {
+            /* ... */
+        }
+    }
+}
+```
---
+++ source/react-unity-webgl.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+require('./react-unity-webgl.css');
+
+const Message = ((gameObjectName, methodName, paramterValue) => {
+    if (paramterValue == null) {
+        paramterValue = "";
+    }
+    if (module.exports.UnityInstance != null) {
+        module.exports.UnityInstance.SendMessage (
+            gameObjectName, 
+            methodName, 
+            paramterValue);
+    }
+});
+
+class Unity extends Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            progress: 0,
+            loaded: false,
+            error: null
+        };
+    }
+    componentDidMount () {
+        this.instantiateUnityLoader ();
+    }
+    instantiateUnityLoader () {
+        if (this.props.src == null) {
+            this.setState({
+                error: "Please provice a path to a valid JSON in the 'src' attribute."
+            });
+            return;
+        }
+        let instance = UnityLoader.instantiate (
+            "unity-container", 
+            this.props.src, {
+                onProgress: ((gameInstance, progress) => {
+                    this.setState({
+                        loaded: progress == 1,
+                        progress: progress
+                    });
+                })
+            });
+        module.exports.UnityInstance = instance;
+    }
+    render() {
+        if (this.state.error == null) {
+            return this.onLoadedRender ();
+        } else {
+            return this.onUnableToRender ();
+        }
+    }
+    onUnableToRender () {
+        return (
+            <div className="unity-container">
+                <b>React-Unity-Webgl error</b>: 
+                {this.state.error}
+            </div>
+        );
+    }
+    onLoadedRender () {
+        return (
+            <div style={this.style}>
+                <div className="unity-container" id="unity-container"></div>
+                {this.state.loaded == false &&
+                    <div className="unity-loader">
+                        <div className="bar">
+                            <div className="fill" style={{ width: (this.state.progress * 100) + "%"}}></div>
+                        </div>
+                    </div>
+                }
+            </div>
+        );
+    }
+}
+
+export {
+    Unity,
+    Message
+};
