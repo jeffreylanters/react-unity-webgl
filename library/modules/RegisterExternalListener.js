9a4f0ea693927781c40502a439ac7420d71b32db
Jeffrey Lanters <jeffrey@hulan.nl>
2017-12-22 17:43:32 +0100

    Updated package

--- library/modules/RegisterExternalListener.js
+++ library/modules/RegisterExternalListener.js
@@ -4,14 +4,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 exports.RegisterExternalListener = RegisterExternalListener;
-/**
- * Registers a listener to this window. When a message is sent
- * from Unity using 'CallExternal', the listener will forward it
- * into your React Application.
- * @param {string} functionName 
- * @param {function} callback 
- */
-function RegisterExternalListener(functionName, callback) {
+function RegisterExternalListener(methodName, callback) {
     /**
      * LEGACY
      *  bind the function to the window to allow
@@ -19,8 +12,8 @@ function RegisterExternalListener(functionName, callback) {
      *  Application.ExternalCall and
      *  Application.ExternalEval.
      */
-    window[functionName] = function (paramterValue) {
-        callback(paramterValue);
+    window[methodName] = function (parameter) {
+        callback(parameter);
     };
 
     /**
@@ -29,7 +22,7 @@ function RegisterExternalListener(functionName, callback) {
      * to make direct calls into React.
      */
     if (typeof window.ReactUnityWebGL === 'undefined') window.ReactUnityWebGL = {};
-    window.ReactUnityWebGL[functionName] = function (paramterValue) {
-        callback(paramterValue);
+    window.ReactUnityWebGL[methodName] = function (parameter) {
+        callback(parameter);
     };
 }
