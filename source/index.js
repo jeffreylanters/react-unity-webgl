6b5a75822f751b33e606664d8733b2c59e546221
Jeffrey Lanters <jeffrey@hulan.nl>
2018-03-16 18:11:09 +0100

    Exports

--- source/index.js
+++ source/index.js
@@ -2,7 +2,7 @@ import Unity from "./components/Unity";
 import { RegisterExternalListener } from "./modules/RegisterExternalListener";
 import { SendMessage } from "./modules/SendMessage";
 import { UnityEvent } from "./modules/UnityEvent";
-import { Fullscreen } from "./modules/Fullscreen";
+import { SetFullscreen } from "./modules/Fullscreen";
 
 export default Unity;
 export { RegisterExternalListener, SendMessage, UnityEvent, SetFullscreen };
