2634952e95b9e979f95dd29fd3bb730a96a9cc63
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-06-23 15:59:32 +0200

    New props on the component

--- source/interfaces/IUnityProps.d.ts
+++ source/interfaces/IUnityProps.d.ts
@@ -4,6 +4,18 @@ export default interface IUnityProps {
      * The content that should be rendered within
      * the Unity component.
      */
-    content: UnityContent;
+    unityContent: UnityContent;
+    /**
+     * The class name for the canvas wrapper.
+     */
+    className?: string;
+    /**
+     * The width of the element.
+     */
+    width?: string | number;
+    /**
+     * The height of the element.
+     */
+    height?: string | number;
 }
 //# sourceMappingURL=IUnityProps.d.ts.map
