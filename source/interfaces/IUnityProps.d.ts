75b6974819247c020fabc7753ab11ace465d1b8c
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-06-23 15:10:16 +0200

    Added Unity content to component props

--- source/interfaces/IUnityProps.d.ts
+++ source/interfaces/IUnityProps.d.ts
@@ -1,5 +1,9 @@
 import UnityContent from "../UnityContent";
 export default interface IUnityProps {
+    /**
+     * The content that should be rendered within
+     * the Unity component.
+     */
     content: UnityContent;
 }
 //# sourceMappingURL=IUnityProps.d.ts.map
