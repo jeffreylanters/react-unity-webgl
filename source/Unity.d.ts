0eb28b6826c2c6974bf41258e3469fd05fd0cc8e
Jeffrey Lanters <jeffreylanters@gmail.com>
2018-06-23 16:01:28 +0200

    Removed old dist files

--- source/Unity.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import IUnityProps from "./interfaces/IUnityProps";
-import IUnityState from "./interfaces/IUnityState";
-export default class Unity extends React.Component<IUnityProps, IUnityState> {
-    constructor(props: IUnityProps);
-    render(): React.ReactNode;
-}
-//# sourceMappingURL=Unity.d.ts.map
