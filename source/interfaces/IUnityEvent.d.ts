2b9c13c1b29cc20f2c2da484a8f4a55fb5988b4a
Jeffrey Lanters <jeffrey@hulan.nl>
2018-06-25 10:49:53 +0200

    Interface for event listeners

---
+++ source/interfaces/IUnityEvent.d.ts
@@ -0,0 +1,14 @@
+export default interface IUnityEvent {
+    /**
+     * The events name. It will be triggered by the name.
+     * @type {string}
+     */
+    eventName: string;
+    /**
+     * The events callback. This event will be triggered
+     * when the
+     * @type {Function}
+     */
+    eventCallback: Function;
+}
+//# sourceMappingURL=IUnityEvent.d.ts.map
