379d9a110efb407df0d1c4109e152ee33bd5e840
Jeffrey Lanters <jeffrey@hulan.nl>
2018-06-25 10:50:03 +0200

    Added types to props

--- source/interfaces/IUnityProps.d.ts
+++ source/interfaces/IUnityProps.d.ts
@@ -3,18 +3,22 @@ export default interface IUnityProps {
     /**
      * The content that should be rendered within
      * the Unity component.
+     * @type {UnityContent}
      */
     unityContent: UnityContent;
     /**
      * The class name for the canvas wrapper.
+     * @type {string}
      */
     className?: string;
     /**
      * The width of the element.
+     * @type {string | number}
      */
     width?: string | number;
     /**
      * The height of the element.
+     * @type {string | number}
      */
     height?: string | number;
 }
