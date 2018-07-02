e8c617a7e187f7f0ce4c98e0a8b8816a95b9789b
Jeffrey Lanters <jeffrey@hulan.nl>
2018-07-02 10:51:19 +0200

    Removed build files

--- source/index.d.ts
+++
@@ -1,6 +0,0 @@
-import IUnityConfig from "./interfaces/IUnityConfig";
-import UnityContent from "./UnityContent";
-import Unity from "./components/Unity";
-import { UnityVersion } from "./enums/UnityVersion";
-export { IUnityConfig, UnityContent, UnityVersion };
-export default Unity;
--- source/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var UnityContent_1 = require("./UnityContent");
-exports.UnityContent = UnityContent_1["default"];
-var Unity_1 = require("./components/Unity");
-var UnityVersion_1 = require("./enums/UnityVersion");
-exports.UnityVersion = UnityVersion_1.UnityVersion;
-exports["default"] = Unity_1["default"];
-//# sourceMappingURL=index.js.map
