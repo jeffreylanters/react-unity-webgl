0ec4e6322d49cce2cd845b907e4ef894104a7e56
Jeffrey Lanters <jeffrey@hulan.nl>
2018-07-02 14:14:09 +0200

    Added pointer storage

--- source/services/UnityLoaderService.ts
+++ source/services/UnityLoaderService.ts
@@ -14,7 +14,7 @@ export default class UnityLoaderService {
    * @private
    */
   private unityLoaderScript?: HTMLScriptElement;
-
+  
   /**
    * Appends the Unity loader script to the window. When it's loaded a callback will
    * be triggered. NOTE: This can't be a promisse due to JavaScript compatibilty.
@@ -23,11 +23,21 @@ export default class UnityLoaderService {
    * @public
    */
   public append(source: string, onLoad: Function): void {
+    // Unity is going to overwrite the console error function. Yeah,
+    // I didn't believe it either. Anyway, we're going to store the
+    // native pointer and restore that after Unity loaded.
+    const _consoleLog = window.console.error;
+
+    // We're going to load the UnityLoader async.
     this.unityLoaderScript = document.createElement("script");
     this.unityLoaderScript.type = "text/javascript";
     this.unityLoaderScript.async = true;
     this.unityLoaderScript.src = source;
     this.unityLoaderScript.onload = () => {
+    
+      // First we restore the console error function back to
+      // the window before starting your Unity player.
+      window.console.error = _consoleLog;
       onLoad();
     };
     this.documentHead.appendChild(this.unityLoaderScript);
