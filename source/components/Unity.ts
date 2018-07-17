55d501ab76348e03c27ce9e1154a7b1daa5bb93d
Jeffrey Lanters <jeffrey@hulan.nl>
2018-07-17 17:40:07 +0200

    Implemented adjustOnWindowResize

--- source/components/Unity.ts
+++ source/components/Unity.ts
@@ -35,12 +35,19 @@ export default class Unity extends React.Component<IUnityProps, IUnityState> {
   public state: IUnityState = {};
 
   /**
+   * The stored binding for the on window
+   * resize listener.
+   */
+  private onWindowResizeBinding: () => void;
+
+  /**
    * Initialized the component.
    * @param {IUnityProps} props
    */
   constructor(props: IUnityProps) {
     super(props);
     this.unityLoaderService = new UnityLoaderService();
+    this.onWindowResizeBinding = this.onWindowResize.bind(this);
     this.unityContent = this.props.unityContent;
     this.unityContent.setComponentInstance(this);
   }
@@ -59,20 +66,61 @@ export default class Unity extends React.Component<IUnityProps, IUnityState> {
   }
 
   /**
+   * When the window is resized.
+   */
+  private onWindowResize(): void {
+    if (this.unityContent.unityConfig.adjustOnWindowResize === true)
+      this.adjustCanvasToContainer();
+  }
+
+  /**
+   * Since the Unity canvas itself does not respond to the resizing
+   * of it's container we have to manually do this. A width and height
+   * of 100% does not seem to work, so we have to fetch it's parent's
+   * size to adject the canvas.
+   * @private
+   */
+  private adjustCanvasToContainer(): void {
+    const _width = this.htmlElement!.offsetWidth;
+    const _height = this.htmlElement!.offsetHeight;
+    const _canvas = this.htmlElement!.getElementsByTagName("canvas")[0];
+    if (_canvas !== null) {
+      if (_canvas.height !== _height) {
+        _canvas.height = _height;
+      }
+      if (_canvas.width !== _width) {
+        _canvas.width = _width;
+      }
+    }
+  }
+
+  /**
    * Initialzied the Unity player when the component is mounted.
    * @public
    */
   public componentDidMount(): void {
+    window.addEventListener("resize", this.onWindowResizeBinding);
     // prettier-ignore
     this.unityLoaderService.append(this.props.unityContent.unityLoaderJsPath, () => {
       this.unityContent.setUnityInstance(UnityLoader.instantiate(
         `__ReactUnityWebGL_${this.props.unityContent.uniqueID}__`,
         this.props.unityContent.buildJsonPath, {
           onProgress: this.onProgress.bind(this),
-          Module: this.props.unityContent.unityConfig.modules
+          Module: this.props.unityContent.unityConfig.modules,
+          width: "100%",
+          height: "100%"
         }));
       }
     );
+  }
+
+  /**
+   * Will remove event listeners and clean up systems when the
+   * component is about to unmount.
+   * @public
+   */
+  public componentWillUnmount(): void {
+    window.removeEventListener("resize", this.onWindowResizeBinding);
   }
 
   /**
