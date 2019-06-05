afd1ec247bfe95a5c214da931abb66c08988c4bd
Jeffrey Lanters <jeffreylanters@gmail.com>
2019-06-05 23:51:27 +0200

    Added additional details to logging

--- source/services/LoggingService.ts
+++ source/services/LoggingService.ts
@@ -1,30 +1,32 @@
 class LoggingService {
-  public warnUnityContentRemoveNotAvailable(): void {
+  public warnUnityContentRemoveNotAvailable(additionalDetials?: any): void {
     this.warn(
       "Your version of Unity does not support unloading the WebGL Player.",
       "This preverts ReactUnityWebGL from unmounting this component properly.",
       "Please consider updating to Unity 2019.1 or newer, or reload the page",
       "to free the WebGL Player from the memory. See the follow link for more details:",
-      "https://github.com/elraccoone/react-unity-webgl/issues/22"
+      "https://github.com/elraccoone/react-unity-webgl/issues/22",
+      additionalDetials
     );
   }
 
-  public errorUnityLoaderNotFound(): void {
-    this.warn(
+  public errorUnityLoaderNotFound(additionalDetials?: any): void {
+    this.error(
       "Unable to use the Unity Loader, please make sure you've imported",
       "the Unity Loader the correct way. You might have entered an incorrect",
       "path to the UnityLoader.js. The path is not relative to your bundle,",
       "but to your index html file. See the follow link for more details: ",
-      "https://github.com/elraccoone/react-unity-webgl/issues/31"
+      "https://github.com/elraccoone/react-unity-webgl/issues/31",
+      additionalDetials
     );
   }
 
   private warn(...messages: string[]): void {
-    console.warn(messages.join(" "));
+    console.warn(messages.filter(_ => typeof _ !== "undefined").join(" "));
   }
 
   private error(...messages: string[]): void {
-    console.error(messages.join(" "));
+    console.error(messages.filter(_ => typeof _ !== "undefined").join(" "));
   }
 }
 
