b56ab43f7b992811d131e585dc3fb9913cb0ee53
Jeffrey Lanters <jeffreylanters@me.com>
2021-03-10 17:05:36 +0100

    Improved UnityLoader instance pooling

--- source/services/unity-loader-service.ts
+++ source/services/unity-loader-service.ts
@@ -1,38 +1,56 @@
 export default class UnityLoaderService {
   /**
+   * A reference to all UnityLoader script tags and their respective urls.
+   * @type {Array<{ url: string; htmlScriptElement: HTMLScriptElement; }>}
+   */
+  private static unityLoaderInstances: Array<{
+    url: string;
+    htmlScriptElement: HTMLScriptElement;
+  }> = new Array();
+  /**
    * A reference to the document head.
    * @type {HTMLHeadElement}
    */
   private documentHead: HTMLHeadElement = document.querySelector("head")!;
 
   /**
-   * A reference to the UnityLoader script.
-   * @type {HTMLScriptElement}
+   * Adds the Unity loader script to the window. When a version of the loader
+   * is already appended, we'll skip to the validation right away. Before
+   * resolving a type check on the createUnityInstance method is done.
+   * @param {string} url the path to the Unity loader file
+   * @returns A promise when resolving when the file is loaded succesfulling.
    */
-  private unityLoaderScript?: HTMLScriptElement;
+  public async addFromUrl(url: string): Promise<void> {
+    let _hasSimilarUnityLoaderUrlInstance = false;
+    for (let _unityLoaderInstance of UnityLoaderService.unityLoaderInstances)
+      if (_unityLoaderInstance.url === url)
+        _hasSimilarUnityLoaderUrlInstance = true;
+    if (_hasSimilarUnityLoaderUrlInstance === false) {
+      const _htmlScriptElement = await this.appendAndLoadScript(url);
+      UnityLoaderService.unityLoaderInstances.push({
+        url,
+        htmlScriptElement: _htmlScriptElement,
+      });
+    }
+    if (typeof createUnityInstance !== "function")
+      throw new Error("Unable to invoke Unity Loader, loaded incorrectly.");
+  }
 
   /**
-   * Appends the Unity loader script to the window. When a version of the loader
-   * is already appended, we'll skip right away. When another loader is try to
-   * append, the previous will be removed first.
-   * @param {string} url the path to the Unity loader file
-   * @returns A promise when resolving when the file is loaded.
+   * Appends a new script tag to the window. The promise resolves when the script
+   * is loaded and rejects when it failed to load.
+   * @param {string} url The url of the script
+   * @returns A promise containing the HTML Script Ele,ent
    */
-  public load(url: string): Promise<HTMLScriptElement> {
+  private appendAndLoadScript(url: string): Promise<HTMLScriptElement> {
     return new Promise<HTMLScriptElement>((resolve, reject) => {
-      if (typeof this.unityLoaderScript !== "undefined")
-        if (url === this.unityLoaderScript.src) {
-          return resolve(this.unityLoaderScript);
-        } else {
-          this.unityLoaderScript.remove();
-        }
-      this.unityLoaderScript = document.createElement("script");
-      this.unityLoaderScript.type = "text/javascript";
-      this.unityLoaderScript.async = true;
-      this.unityLoaderScript.src = url;
-      this.unityLoaderScript.onload = () => resolve(this.unityLoaderScript!);
-      this.unityLoaderScript.onerror = () => reject(`Unable to load ${url}`);
-      this.documentHead.appendChild(this.unityLoaderScript);
+      var _scriptTag = document.createElement("script");
+      _scriptTag.type = "text/javascript";
+      _scriptTag.async = true;
+      _scriptTag.src = url;
+      _scriptTag.onload = () => resolve(_scriptTag!);
+      _scriptTag.onerror = (error) => reject(`Unable to load ${url} ${error}`);
+      this.documentHead.appendChild(_scriptTag);
     });
   }
 }
