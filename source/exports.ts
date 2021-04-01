0ae58f41c94438308fff5507ab9a117b48563c17
Jeffrey Lanters <jeffreylanters@me.com>
2021-04-01 22:36:05 +0200

    Added Unity Config to exports

--- source/exports.ts
+++ source/exports.ts
@@ -1,5 +1,6 @@
 import Unity from "./components/unity";
 import UnityContext from "./models/unity-context";
+import UnityConfig from "./interfaces/unity-config";
 
 export default Unity;
-export { UnityContext };
+export { UnityContext, UnityConfig };
