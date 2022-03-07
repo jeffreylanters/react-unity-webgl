497f144790513775fdf5b307d12477e1132c7b5b
Jeffrey Lanters <jeffreylanters@me.com>
2022-03-07 10:20:19 +0100

    Minor styling changes

--- source/interfaces/webgl-context-attributes.ts
+++ source/interfaces/webgl-context-attributes.ts
@@ -5,7 +5,7 @@
  */
 export interface IWebGLContextAttributes {
   /**
-   * If set to true, the context contains an alpha (transparency) buffer. 
+   * If set to true, the context contains an alpha (transparency) buffer.
    * @public
    * @readonly
    * @type {boolean}
@@ -21,26 +21,27 @@ export interface IWebGLContextAttributes {
    * @default true
    */
   readonly depth?: boolean;
-  
+
   /**
-   * If set to true, the context is requested to have a stencil buffer of at least 8 bits. 
+   * If set to true, the context is requested to have a stencil buffer of at
+   * least 8 bits.
    * @public
    * @readonly
    * @type {boolean}
    * @default false
    */
   readonly stencil?: boolean;
-  
+
   /**
-   * If set to true, this hints the user agent (browser) to reduce the latency by 
-   * desynchronizing the context paint cycle from the event loop
+   * If set to true, this hints the user agent (browser) to reduce the latency
+   * by desynchronizing the context paint cycle from the event loop.
    * @public
    * @readonly
    * @type {boolean}
    * @default false
    */
   readonly desynchronized?: boolean;
-  
+
   /**
    * If set to true, the context will attempt to perform antialiased rendering
    * if possible.
@@ -68,29 +69,29 @@ export interface IWebGLContextAttributes {
   readonly failIfMajorPerformanceCaveat?: boolean;
 
   /**
-   * Provides a hint to the user agent (browser) indicating what configuration of GPU is
-   * suitable for this WebGL context. This may influence which GPU is used in a
-   * system with multiple GPUs. For example, a dual-GPU system might have one
-   * GPU that consumes less power at the expense of rendering performance.
-   * Note that this property is only a hint and a WebGL implementation may
-   * choose to ignore it. WebGL implementations use context lost and restored
-   * events to regulate power and memory consumption, regardless of the value of
-   * this attribute.
-   * 
-   * "default": Let the user agent (browser) decide which GPU configuration is most suitable.
-   * "high-performance": Prioritizes rendering performance over power consumption.
-   * "low-power": Prioritizes power saving over rendering performance.
-   *
+   * Provides a hint to the user agent (browser) indicating what configuration
+   * of GPU is suitable for this WebGL context. This may influence which GPU is
+   * used in a system with multiple GPUs. For example, a dual-GPU system might
+   * have one GPU that consumes less power at the expense of rendering
+   * performance. Note that this property is only a hint and a WebGL
+   * implementation may choose to ignore it. WebGL implementations use context
+   * lost and restoredevents to regulate power and memory consumption,
+   * regardless of the value of this attribute.
+   * - default: Let the user agent (browser) decide which GPU configuration is
+   *   most suitable.
+   * - high-performance: Prioritizes rendering performance over power
+   *   consumption.
+   * - low-power: Prioritizes power saving over rendering performance.
    * @public
    * @readonly
-   * @type {boolean}
+   * @type {"default" | "high-performance" | "low-power"}
    * @default "default"
    */
   readonly powerPreference?: "default" | "high-performance" | "low-power";
 
   /**
-   * If set to true, page compositor will assume the drawing buffer contains colors 
-   * with pre-multiplied alpha. (The color channels in the framebuffer should be 
+   * If set to true, page compositor will assume the drawing buffer contains colors
+   * with pre-multiplied alpha. (The color channels in the framebuffer should be
    * stored premultipled by the alpha channel to improve performance.)
    * @public
    * @readonly
@@ -100,7 +101,7 @@ export interface IWebGLContextAttributes {
   readonly premultipliedAlpha?: boolean;
 
   /**
-   * If set to true, the buffers will not be cleared and will preserve their values 
+   * If set to true, the buffers will not be cleared and will preserve their values
    * until cleared or overwritten by the author. If you wish to use canvas.toDataURL()
    * you will either need to draw to the canvas immediately before calling toDataURL()
    * or set preserveDrawingBuffer to true to keep the buffer available after the
@@ -114,9 +115,9 @@ export interface IWebGLContextAttributes {
 
   /**
    * If set to true, this hints to the user agent (browser) to use a compatible graphics
-   * adapter for an immersive XR device. Setting this synchronous flag at context creation 
+   * adapter for an immersive XR device. Setting this synchronous flag at context creation
    * is discouraged; rather call the asynchronous WebGLRenderingContext.makeXRCompatible()
-   * method the moment you intend to start an XR session. 
+   * method the moment you intend to start an XR session.
    * @public
    * @readonly
    * @type {boolean}
