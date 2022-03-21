6ebff8e6c2915a04f9943b50988614f52e0e3b42
Jeffrey Lanters <jeffreylanters@me.com>
2022-03-21 14:03:44 +0100

    Added testing app

---
+++ testing/src/app.tsx
@@ -0,0 +1,24 @@
+import { Fragment, useState } from "react";
+import { UnityTest } from "./unity-test";
+
+function App() {
+  const [isUnityTestMounted, setIsUnityTestMounted] = useState(false);
+  const [repaintCounter, setRepaintCounter] = useState(0);
+
+  console.log("App is being painted!");
+
+  return (
+    <Fragment>
+      <h1>App</h1>
+      <button onClick={() => setIsUnityTestMounted((s) => !s)}>
+        (Un)mount Unity Test
+      </button>
+      <button onClick={() => setRepaintCounter((i) => i + 1)}>
+        Force Repaint ({repaintCounter})
+      </button>
+      {isUnityTestMounted === true && <UnityTest />}
+    </Fragment>
+  );
+}
+
+export { App };
---
+++ testing/src/unity-test.tsx
@@ -0,0 +1,42 @@
+import { FunctionComponent } from "react";
+import { Unity, useUnityContext } from "../../module/distribution/exports";
+
+const UnityTest: FunctionComponent = () => {
+  const {
+    loadingProgression,
+    unityProvider,
+    isLoaded,
+    setFullscreen,
+    sendMessage,
+  } = useUnityContext({
+    codeUrl: "/unitybuild/myunityapp.wasm",
+    dataUrl: "/unitybuild/myunityapp.data",
+    frameworkUrl: "/unitybuild/myunityapp.framework.js",
+    loaderUrl: "/unitybuild/myunityapp.loader.js",
+  });
+
+  return (
+    <div>
+      <h2>Unity Test</h2>
+      <p>Loading progression: {loadingProgression}</p>
+      <p>Loaded?: {isLoaded ? "Y" : "N"}</p>
+      <div>
+        <button onClick={() => setFullscreen(true)}>Fullscreen</button>
+        <button
+          children="Normal"
+          onClick={() => sendMessage("MeshCrate", "SetRotationSpeed", 50)}
+        />
+        <button
+          children="Fast"
+          onClick={() => sendMessage("MeshCrate", "SetRotationSpeed", 200)}
+        />
+      </div>
+      {/* <Unity
+        unityProvider={unityProvider}
+        style={{ border: "1px solid red", height: 300, width: 400 }}
+      /> */}
+    </div>
+  );
+};
+
+export { UnityTest };
