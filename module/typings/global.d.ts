face876cb0f79f014b3de377ff3e75c52b799cc0
Jeffrey Lanters <jeffreylanters@me.com>
2022-03-22 17:32:32 +0100

    Exposed dispatch and create unity instance events to the window as well

--- module/typings/global.d.ts
+++ module/typings/global.d.ts
@@ -26,4 +26,27 @@ declare global {
     arguments: IUnityArguments,
     onProgress?: (progression: number) => void
   ): Promise<UnityInstance>;
+
+  /**
+   * Due to some developers wanting to use the window object as a global scope
+   * in order to invoke the create Unity Instance and dispatch React Unity Event
+   * functions, we need to declare the window object as a global type.
+   */
+  interface Window {
+    /**
+     * Dispatches an event that has been registered to all event systems.
+     * @param eventName The name of the event.
+     * @param arguments The arguments to pass to the event.
+     */
+    dispatchReactUnityEvent: typeof dispatchReactUnityEvent;
+
+    /**
+     * Creates a new UnityInstance.
+     * @param canvasHtmlElement The target html canvas element.
+     * @param arguments The arguments needed to load Unity.
+     * @param onProgress The on progress event listener.
+     * @returns A promise resolving when instantiated successfully.
+     */
+    createUnityInstance: typeof createUnityInstance;
+  }
 }
