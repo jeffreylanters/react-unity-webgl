98b54110ac226b1e892ea11872f30456772e9ef8
Jeffrey Lanters <jeffreylanters@me.com>
2022-03-22 17:13:52 +0100

    Added types for React Unity event arguments

---
+++ module/typings/react-unity-event-argument-type.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Type definition for Unity event argument types. These are the supported
+ * types for the React Unity WebGL event system which can be used to dispatch
+ * events to the React Application from the Unity Instance.
+ */
+declare type ReactUnityEventArgumentType = string | number;
