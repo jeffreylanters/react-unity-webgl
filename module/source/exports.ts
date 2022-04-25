2a8fa12147a262a269b7580f858e36d5b2a4ad2f
Jeffrey Lanters <jeffreylanters@me.com>
2022-04-25 18:05:25 +0200

    Added some unfull interfaces to the module exports

--- module/source/exports.ts
+++ module/source/exports.ts
@@ -1,5 +1,8 @@
 import { Unity } from "./components/unity-component";
 import { useUnityContext } from "./hooks/use-unity-context";
+import { IUnityConfig } from "./interfaces/unity-config";
+import { IUnityProps } from "./interfaces/unity-props";
+import { IWebGLContextAttributes } from "./interfaces/webgl-context-attributes";
 
-export default Unity;
 export { Unity, useUnityContext };
+export type { IUnityConfig, IUnityProps, IWebGLContextAttributes };
