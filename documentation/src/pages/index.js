e36a9d58f1762a6c49937dc3e847c987803fd53f
Jeffrey Lanters <jeffreylanters@me.com>
2022-05-02 17:57:31 +0200

    Updated styling

---
+++ documentation/src/pages/index.js
@@ -0,0 +1,21 @@
+import Layout from "@theme/Layout";
+import React from "react";
+import styles from "./index.module.css";
+
+const Index = () => (
+  <Layout
+    title={`React Unity WebGL`}
+    description="Description will go into a meta tag in <head />"
+  >
+    <main className={styles.main}>
+      <img src="/images/logo.svg" alt="React Unity WebGL Logo" />
+      <h1>React Unity WebGL</h1>
+      <p>Bring your Unity Games to the Web!</p>
+      <a href="/docs/introduction">
+        <button>Get Started</button>
+      </a>
+    </main>
+  </Layout>
+);
+
+export default Index;
