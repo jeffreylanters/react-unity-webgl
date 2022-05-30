937a89d6b6febd705a9dac9ef501c85298d43d3f
Jeffrey Lanters <jeffreylanters@me.com>
2022-05-30 12:45:10 +0200

    Implemented theme in index

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -1,21 +1,30 @@
 import Layout from "@theme/Layout";
 import React from "react";
 import styles from "./index.module.css";
+import ThemedImage from "@theme/ThemedImage";
 
-const Index = () => (
-  <Layout
-    title={`React Unity WebGL`}
-    description="Bring your Unity Games to the Web!"
-  >
-    <main className={styles.main}>
-      <img src="/images/logo.svg" alt="React Unity WebGL Logo" />
-      <h1>React Unity WebGL</h1>
-      <p>Bring your Unity Games to the Web!</p>
-      <a href="/docs/introduction">
-        <button>Get Started</button>
-      </a>
-    </main>
-  </Layout>
-);
+const Index = () => {
+  return (
+    <Layout
+      title={`React Unity WebGL`}
+      description="Bring your Unity Games to the Web!"
+    >
+      <main className={styles.main}>
+        <ThemedImage
+          alt={"React Unity WebGL Logo"}
+          sources={{
+            light: "/images/logo.svg",
+            dark: "/images/logo-light.svg",
+          }}
+        />
+        <h1>React Unity WebGL</h1>
+        <p>Bring your Unity Games to the Web!</p>
+        <a href="/docs/introduction">
+          <button>Get Started</button>
+        </a>
+      </main>
+    </Layout>
+  );
+};
 
 export default Index;
