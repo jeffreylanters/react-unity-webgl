97a99f1720f6c543c0169b87855c175a40286ffe
Jeffrey Lanters <jeffreylanters@me.com>
2022-06-03 13:59:44 +0200

    Added sitemap

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -31,6 +31,11 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        sitemap: {
+          changefreq: "weekly",
+          priority: 0.5,
+          ignorePatterns: [],
+        },
       }),
     ],
   ],
