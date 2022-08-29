8d924902b1ac4609177bf9b568f27b3d45e95dba
Jeffrey Lanters <jeffreylanters@me.com>
2022-08-29 15:02:07 +0200

    Updated Unity Test Project

---
+++ testing/project/Assets/Controller.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine.UI;
+using UnityEngine;
+
+class Controller : MonoBehaviour {
+  [DllImport ("__Internal")] static extern void ClickedTestButton ();
+  [DllImport ("__Internal")] static extern void ClickedStringTestButton (string value);
+  [DllImport ("__Internal")] static extern void ClickedNumberTestButton (int value);
+  [DllImport ("__Internal")] static extern void ClickedNumbersTestButton (int[] values, int length);
+  [DllImport ("__Internal")] static extern void ClickedBoolTestButton (bool value);
+  [DllImport ("__Internal")] static extern void ClickedObjectTestButton (string stringValue, int intValue, bool boolValue);
+
+  public Text textLog;
+
+  public void DispatchEvent () { 
+#if !UNITY_EDITOR && UNITY_WEBGL
+    ClickedTestButton(); 
+#endif
+  }
+
+  public void DispatchStringEvent () { 
+#if !UNITY_EDITOR && UNITY_WEBGL
+    ClickedStringTestButton("Hello World!"); 
+#endif
+  }
+
+  public void DispatchNumberEvent () { 
+#if !UNITY_EDITOR && UNITY_WEBGL
+    ClickedNumberTestButton(42); 
+#endif
+  }
+
+  public void DispatchNumbersEvent () { 
+#if !UNITY_EDITOR && UNITY_WEBGL
+    var values = new int[] { 1, 2, 3, 4, 5 };
+    ClickedNumbersTestButton(values, values.Length); 
+#endif
+  }
+
+  public void DispatchBoolEvent () { 
+#if !UNITY_EDITOR && UNITY_WEBGL
+    ClickedBoolTestButton(true); 
+#endif
+  }
+
+  public void DispatchObjectEvent () { 
+#if !UNITY_EDITOR && UNITY_WEBGL
+    ClickedObjectTestButton("Hello World!", 42, true); 
+#endif
+  }
+
+  void SetLogText (string text) {
+    textLog.text = text;
+  }
+}
---
+++ testing/project/Assets/Plugins/WebGL/Bridge.jslib
@@ -0,0 +1,36 @@
+﻿
+  [DllImport ("__Internal")] static extern void ClickedTestButton ();
+  [DllImport ("__Internal")] static extern void ClickedStringTestButton (string value);
+  [DllImport ("__Internal")] static extern void ClickedNumberTestButton (int value);
+  [DllImport ("__Internal")] static extern void ClickedNumbersTestButton (int[] values);
+  [DllImport ("__Internal")] static extern void ClickedBoolTestButton (bool value);
+  [DllImport ("__Internal")] static extern void ClickedObjectTestButton (string stringValue, int intValue, bool boolValue);
+
+mergeInto(LibraryManager.library, {
+  ClickedTestButton: function() {
+    dispatchReactUnityEvent("ClickedTestButton");
+  },
+  ClickedStringTestButton: function(value) {
+    dispatchReactUnityEvent("ClickedStringTestButton", UTF8ToString(value));
+  },
+  ClickedNumberTestButton: function(value) {
+    dispatchReactUnityEvent("ClickedNumberTestButton", value);
+  },
+  ClickedNumbersTestButton: function(values, length) {
+    var array = new Array(length);
+    for(var i = 0; i < size; i++) {
+      array[i] = HEAPF32[(array >> 2) + i]
+    }
+    dispatchReactUnityEvent("ClickedNumbersTestButton", array);
+  },
+  ClickedBoolTestButton: function(value) {
+    dispatchReactUnityEvent("ClickedBoolTestButton", value);
+  },
+  ClickedObjectTestButton: function(stringValue, intValue, boolValue) {
+    dispatchReactUnityEvent("ClickedObjectTestButton", {
+      stringValue: UTF8ToString(stringValue),
+      intValue: intValue,
+      boolValue: boolValue
+    });
+  }
+});
