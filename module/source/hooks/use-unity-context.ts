37fee33264f01a7b404ed2f1fc223cd0a496a4c9
Jeffrey Lanters <jeffreylanters@me.com>
2022-09-22 15:24:56 +0200

    Minor improvements

--- module/source/hooks/use-unity-context.ts
+++ module/source/hooks/use-unity-context.ts
@@ -179,13 +179,14 @@ const useUnityContext = (unityConfig: IUnityConfig): IUnityContextHook => {
       // Re-attaches the canvas to the body element of the document. This way it
       // wont be removed from the DOM when the component is unmounted. Then the
       // canvas will be hidden while it is being unloaded.
-      document.body.appendChild(unityInstance.Module.canvas as Node);
-      unityInstance.Module.canvas.style.display = "none";
+      const canvas = unityInstance.Module.canvas as HTMLCanvasElement;
+      document.body.appendChild(canvas);
+      canvas.style.display = "none";
       // Unloads the Unity Instance.
       await unload();
       // Eventually the canvas will be removed from the DOM. This has to be done
       // manually since the canvas is no longer controlled by the React DOM.
-      unityInstance.Module.canvas.remove();
+      canvas.remove();
     },
     [unityInstance]
   );
