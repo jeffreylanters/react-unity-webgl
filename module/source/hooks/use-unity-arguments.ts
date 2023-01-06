fa8dd52f3ee2bd4ea03abfe85788d5296094934b
Jeffrey Lanters <jeffreylanters@me.com>
2023-01-06 13:00:30 +0100

    Minor argument improvements

--- module/source/hooks/use-unity-arguments.ts
+++ module/source/hooks/use-unity-arguments.ts
@@ -14,6 +14,28 @@ const useUnityArguments = (unityProps: UnityProps): UnityArguments => {
       ...unityProps.unityProvider.unityConfig,
 
       /**
+       * When the device pixel ratio is defined via the Unity Props, it's value
+       * will be appended to the Unity arguments object.
+       */
+      devicePixelRatio: unityProps.devicePixelRatio,
+
+      /**
+       * When the match WebGL to Canvas Size boolean is defined via the Unity
+       * Props, it's value will be appended to the Unity arguments object.
+       */
+      matchWebGLToCanvasSize: unityProps.matchWebGLToCanvasSize,
+
+      /**
+       * Even though the WebGL Context Attributes are defined in the Unity
+       * Config and are spread into the Unity Arguments object, a check is still
+       * performed to see if the WebGL Context Attributes are not undefined
+       * since this would cause the Unity Instance to throw an error. An empty
+       * object is used as a fallback.
+       */
+      webglContextAttributes:
+        unityProps.unityProvider.unityConfig.webglContextAttributes || {},
+
+      /**
        * Print event hooks will be intercepted in order to catch messages and send
        * them to the unity context instead.
        * @param message The message to be printed.
@@ -32,25 +54,6 @@ const useUnityArguments = (unityProps: UnityProps): UnityArguments => {
         // TODO -- Re-implement this hook.
         // unityContext.dispatchEvent("error", error);
       },
-
-      /**
-       * When the device pixel ratio is defined via the Unity Props, it's value
-       * will be appended to the Unity arguments object.
-       */
-      devicePixelRatio: unityProps.devicePixelRatio,
-
-      /**
-       * When the match WebGL to Canvas Size boolean is defined via the Unity
-       * Props, it's value will be appended to the Unity arguments object.
-       */
-      matchWebGLToCanvasSize: unityProps.matchWebGLToCanvasSize,
-
-      /**
-       * When the WebGL Context Attributes object is defined via the Unity
-       * Config, it's value will be appended to the Unity arguments object.
-       */
-      webglContextAttributes:
-        unityProps.unityProvider.unityConfig.webglContextAttributes || {},
     }),
     [
       unityProps.devicePixelRatio,
