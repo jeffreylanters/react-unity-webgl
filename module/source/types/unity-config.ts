6596eadece93b0a5463973d4cdd38e3d232b15fe
Jeffrey Lanters <jeffreylanters@me.com>
2023-01-06 15:36:21 +0100

    Refactored disabled canvas events option from config to props

--- module/source/types/unity-config.ts
+++ module/source/types/unity-config.ts
@@ -17,7 +17,6 @@ type ConfigurableUnityArguments = Pick<
   | "productName"
   | "productVersion"
   | "webglContextAttributes"
-  | "disabledCanvasEvents"
   | "cacheControl"
 >;
 
--- module/source/types/unity-props.ts
+++ module/source/types/unity-props.ts
@@ -1,10 +1,21 @@
 import { CSSProperties } from "react";
 import { UnityProvider } from "./unity-provider";
+import { UnityArguments } from "./unity-arguments";
+
+/**
+ * Some of the Unity Props' properties are also part of the Unity Arguments.
+ * This type is used to pick the properties that are configurable from the
+ * Unity Arguments.
+ */
+type ConfigurableUnityArguments = Pick<
+  UnityArguments,
+  "devicePixelRatio" | "matchWebGLToCanvasSize" | "disabledCanvasEvents"
+>;
 
 /**
  * The Unity component's props.
  */
-type UnityProps = {
+type UnityProps = ConfigurableUnityArguments & {
   /**
    * The Provider of the Unity Context which should be rendered be the Unity
    * Component.
@@ -28,23 +39,6 @@ type UnityProps = {
    * @see https://stackoverflow.com/a/60854680
    */
   readonly tabIndex?: number;
-
-  /**
-   * The Canvas can appear too blurry on retina screens. The devicePixelRatio
-   * determines how much extra pixel density should be added to allow for a
-   * sharper image.
-   * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
-   */
-  readonly devicePixelRatio?: number;
-
-  /**
-   * When disabling the match WebGL to canvas size flag, the canvas allows for
-   * client side customization of the WebGL canvas target size instead of
-   * requiring it to always match 1:1 with the High DPI CSS size of the canvas.
-   * Supported since Unity 2021.1.0b8
-   * @see https://issuetracker.unity3d.com/issues/webgl-builds-dont-allow-separate-control-on-canvas-render-buffer-size
-   */
-  readonly matchWebGLToCanvasSize?: boolean;
 };
 
 export type { UnityProps };
