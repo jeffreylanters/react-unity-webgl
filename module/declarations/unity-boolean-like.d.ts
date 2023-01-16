42a05ff803ab8650753d793e66448011f3c77c64
Jeffrey Lanters <jeffreylanters@me.com>
2023-01-16 11:51:26 +0100

    Added type from Untity boolean like

---
+++ module/declarations/unity-boolean-like.d.ts
@@ -0,0 +1,13 @@
+/**
+ * Unity Boolean Like Type Declaration. This type declaration is used to
+ * represent the boolean values `true` and `false` in the Unity Module.
+ */
+declare type UnityBooleanLike =
+  /**
+   * Represents the boolean value `false`.
+   */
+  | 0
+  /**
+   * Represents the boolean value `true`.
+   */
+  | 1;
