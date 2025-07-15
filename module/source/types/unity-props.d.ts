0b336be314c13b93f3830c6ffa9ef0f7bd9a4744
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 15:39:48 +0200

    Add type declaration for UnityProps to define properties for Unity component

---
+++ module/source/types/unity-props.d.ts
@@ -0,0 +1,24 @@
+import { CSSProperties } from "react";
+import { UnityProvider } from "./unity-provider";
+
+type UnityProps = {
+  /**
+   * The Unity provider that contains the necessary URLs to load the Unity
+   * instance. This is required to load the Unity WebGL build.
+   */
+  unityProvider: UnityProvider;
+
+  /**
+   * The ID of the Unity canvas element. If not provided, a unique ID will be
+   * generated. It is recommended to let the library generate a unique ID
+   * unless you have a specific reason to use a custom ID.
+   */
+  id?: string;
+
+  /**
+   * The style of the Unity canvas element.
+   */
+  style?: CSSProperties;
+};
+
+export type { UnityProps };
