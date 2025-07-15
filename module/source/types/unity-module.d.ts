b64ca0b5527a1425f8ebd7c2f019fd1d8c6214d2
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 15:39:43 +0200

    Add type declaration for UnityModule to define its methods and properties

---
+++ module/source/types/unity-module.d.ts
@@ -0,0 +1,33 @@
+import { UnityBooleanLike } from "./unity-boolean-like";
+
+/**
+ * Type declaration for the UnityModule.
+ */
+type UnityModule = {
+  /**
+   * Stringifies a pointer to a string.
+   * @param pointer The pointer to the string.
+   * @param length The length of the string.
+   * @deprecated Deprecated in Unity 2021.2, use UTF8ToString instead.
+   */
+  Pointer_stringify(pointer: number, length: number): string;
+
+  /**
+   * Converts a pointer to a string.
+   * @param pointer The pointer to the string.
+   */
+  UTF8ToString(pointer: number): string;
+
+  /**
+   * Enables or disabled the fullscreen mode of the UnityInstance.
+   * @param fullScreen sets the fullscreen mode.
+   */
+  SetFullscreen(fullScreen: UnityBooleanLike): void;
+
+  /**
+   * A reference to the Unity Instance's Canvas.
+   */
+  canvas?: HTMLCanvasElement;
+};
+
+export type { UnityModule };
