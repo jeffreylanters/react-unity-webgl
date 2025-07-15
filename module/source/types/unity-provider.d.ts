29b8d5c6b4d03fe40917b43932db1d8cd62b7590
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 16:00:23 +0200

    Add setLoadingProgression method to UnityProvider type for loading feedback

--- module/source/types/unity-provider.d.ts
+++ module/source/types/unity-provider.d.ts
@@ -3,6 +3,11 @@ import { UnityConfig } from "./unity-config";
 type UnityProvider = Pick<
   UnityConfig,
   "loaderUrl" | "dataUrl" | "frameworkUrl" | "codeUrl"
-> & {};
+> & {
+  /**
+   * Sets the Unity Context's loading progression.
+   */
+  readonly setLoadingProgression: (loadingProgression: number) => void;
+};
 
 export type { UnityProvider };
