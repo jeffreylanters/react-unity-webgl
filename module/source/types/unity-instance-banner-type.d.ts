1d0066a19df73e1f6926738811b43fd4303f8987
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 15:39:33 +0200

    Add type definition for UnityInstanceBannerType to represent banner messages

---
+++ module/source/types/unity-instance-banner-type.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Banners can be used to display non-critical warnings and error messages from
+ * the Unity Instance.
+ */
+type UnityInstanceBannerType = "error" | "warning";
+
+export type { UnityInstanceBannerType };
