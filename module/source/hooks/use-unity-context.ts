b4918f22e95e37f3230751f3a70f414a9e013b9c
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 16:00:15 +0200

    Add loading progression state to useUnityContext for enhanced loading feedback

--- module/source/hooks/use-unity-context.ts
+++ module/source/hooks/use-unity-context.ts
@@ -1,18 +1,22 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { UnityConfig } from "../types/unity-config";
 import { UnityContext } from "../types/unity-context";
 import { UnityProvider } from "../types/unity-provider";
 
 const useUnityContext = (unityConfig: UnityConfig): UnityContext => {
+  const [loadingProgression, setLoadingProgression] = useState(0);
+
   const unityProvider = useRef<UnityProvider>({
     codeUrl: unityConfig.codeUrl,
     dataUrl: unityConfig.dataUrl,
     frameworkUrl: unityConfig.frameworkUrl,
     loaderUrl: unityConfig.loaderUrl,
+    setLoadingProgression: setLoadingProgression,
   });
 
   return {
     unityProvider: unityProvider.current,
+    loadingProgression,
   };
 };
 
