bd191de5a125d23a1322e78964e72221e8335afe
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 15:27:04 +0200

    Refactor Unity component to use forwardRef and improve type safety

--- module/source/components/unity.ts
+++ module/source/components/unity.ts
@@ -1,56 +1,86 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { useState, useEffect, createElement } from "react";
+import {
+  useState,
+  useEffect,
+  createElement,
+  forwardRef,
+  ForwardRefExoticComponent,
+  JSX,
+  useImperativeHandle,
+} from "react";
+import { UnityInstance } from "../types/unity-instance";
+import { UnityProps } from "../types/unity-props";
+import { useCanvasIdentifier } from "../hooks/use-canvas-identifier";
 
-let canvasCounter = 0;
+const Unity: ForwardRefExoticComponent<
+  UnityProps & React.RefAttributes<HTMLCanvasElement>
+> = forwardRef<HTMLCanvasElement, UnityProps>(
+  /**
+   * @param unityProps The Unity props provided the the Unity component.
+   * @param forwardedRef The forwarded ref to the Unity component.
+   * @returns The Unity canvas renderer.
+   */
+  (props, forwardedRef): JSX.Element => {
+    const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null);
+    const [unityInstance, setUnityInstance] = useState<UnityInstance | null>(
+      null
+    );
 
-export const Unity = () => {
-  const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement | null>(null);
-  const [unityInstance, setUnityInstance] = useState<any>(null);
-  const [canvasId, setCanvasId] = useState<string>("react-unity-webgl");
+    const [canvasId, refreshCanvasId] = useCanvasIdentifier(props.id);
 
-  useEffect(() => {
-    const initializeUnity = async () => {
-      if (!canvasRef || unityInstance) {
-        return;
-      }
+    useEffect(() => {
+      const initializeUnity = async () => {
+        if (!canvasRef || unityInstance) {
+          return;
+        }
 
-      setCanvasId(`react-unity-webgl-${++canvasCounter}`);
+        refreshCanvasId();
 
-      const unityConfig = {
-        dataUrl: "/unity-build/unity.data",
-        frameworkUrl: "/unity-build/unity.framework.js",
-        codeUrl: "/unity-build/unity.wasm",
+        const unityConfig = {
+          dataUrl: props.unityProvider.dataUrl,
+          frameworkUrl: props.unityProvider.frameworkUrl,
+          codeUrl: props.unityProvider.codeUrl,
+        };
+
+        // TODO: Inject loader script
+
+        setUnityInstance(
+          await window.createUnityInstance(canvasRef, unityConfig)
+        );
       };
 
-      setUnityInstance(
-        await window.createUnityInstance(canvasRef, unityConfig)
-      );
-    };
+      const detachUnity = async () => {
+        if (!unityInstance || !canvasRef) {
+          return;
+        }
 
-    const detachUnity = async () => {
-      if (!unityInstance || !canvasRef) {
-        return;
-      }
+        const cleanupCanvasRef = document.createElement("canvas");
+        cleanupCanvasRef.id = canvasRef.id;
+        cleanupCanvasRef.setAttribute("react-unity-webgl-role", "cleanup");
+        document.body.appendChild(cleanupCanvasRef);
+        unityInstance.Module.canvas = cleanupCanvasRef;
+        setUnityInstance(null);
+        await unityInstance.Quit();
+        document.body.removeChild(cleanupCanvasRef);
+      };
 
-      const cleanupCanvasRef = document.createElement("canvas");
-      cleanupCanvasRef.id = canvasRef.id;
-      cleanupCanvasRef.setAttribute("react-unity-webgl-role", "cleanup");
-      document.body.appendChild(cleanupCanvasRef);
-      unityInstance.Module.canvas = cleanupCanvasRef;
-      setUnityInstance(null);
-      await unityInstance.Quit();
-      document.body.removeChild(cleanupCanvasRef);
-    };
+      initializeUnity();
 
-    initializeUnity();
+      return () => {
+        detachUnity();
+      };
+    }, [canvasRef, unityInstance]);
 
-    return () => {
-      detachUnity();
-    };
-  }, [canvasRef, unityInstance]);
+    useImperativeHandle<HTMLCanvasElement | null, HTMLCanvasElement | null>(
+      forwardedRef,
+      () => canvasRef
+    );
 
-  return createElement("canvas", {
-    ref: setCanvasRef,
-    id: canvasId,
-  });
-};
+    return createElement("canvas", {
+      ref: setCanvasRef,
+      id: canvasId,
+      style: props.style,
+    });
+  }
+);
+
+export { Unity };
