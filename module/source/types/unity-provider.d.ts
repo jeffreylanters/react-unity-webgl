54b8c125910c67f478efed5b159a875db9ebada7
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 16:12:09 +0200

    Add optional setIsLoaded method to UnityProvider type for loading state management

--- module/source/types/unity-provider.d.ts
+++ module/source/types/unity-provider.d.ts
@@ -8,6 +8,11 @@ type UnityProvider = Pick<
    * Sets the Unity Context's loading progression.
    */
   readonly setLoadingProgression: (loadingProgression: number) => void;
+
+  /**
+   * Sets the Unity Context's loaded state.
+   */
+  readonly setIsLoaded?: (isLoaded: boolean) => void;
 };
 
 export type { UnityProvider };
