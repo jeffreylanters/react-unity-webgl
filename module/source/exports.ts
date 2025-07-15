21255e051f3c0e71ce5b7586fd5a6f394f78d5f2
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 11:54:21 +0200

    Removed unused exports

--- module/source/exports.ts
+++ module/source/exports.ts
@@ -1,8 +1,3 @@
-import { Unity } from "./components/unity-component";
-import { useUnityContext } from "./hooks/use-unity-context";
-import { UnityConfig } from "./types/unity-config";
-import { UnityProps } from "./types/unity-props";
-import { WebGLContextAttributes } from "./types/webgl-context-attributes";
+import { Unity } from "./components/unity";
 
-export { Unity, useUnityContext };
-export type { UnityConfig, UnityProps, WebGLContextAttributes };
+export { Unity };
