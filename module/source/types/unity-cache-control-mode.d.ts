afefba742319106cea0785eec9c412946d46f8d2
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 15:39:23 +0200

    Add UnityCacheControlMode type definition to manage caching behavior in Unity

---
+++ module/source/types/unity-cache-control-mode.d.ts
@@ -0,0 +1,28 @@
+/**
+ * The cache control mode determines how the Unity should cache the resource.
+ * - `must-revalidate`: The cache returns to an enabled state and the file is
+ *   revalidated before being loaded from the cache.
+ * - `immutable`: the cache is enabled and the file is loaded from the cache
+ *   without revalidation.
+ * - `no-store`: The cache is disabled.
+ */
+type UnityCacheControlMode =
+  /**
+   * The cache returns to an enabled state and the file is revalidated before
+   * being loaded from the cache.
+   */
+  | "must-revalidate"
+  /**
+   * the cache is enabled and the file is loaded from the cache without revalidation.
+   */
+  | "immutable"
+  /**
+   * The cache is disabled.
+   */
+  | "no-store"
+  /**
+   * Fallback for when the cache control mode is not recognized.
+   */
+  | string;
+
+export type { UnityCacheControlMode };
