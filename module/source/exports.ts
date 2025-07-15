e422dc7bd28a4a060b92e58a1b2e723ea38986fa
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 16:12:12 +0200

    Export UnityEventParameter type from exports.ts for improved type accessibility

--- module/source/exports.ts
+++ module/source/exports.ts
@@ -1,4 +1,6 @@
 import { Unity } from "./components/unity";
 import { useUnityContext } from "./hooks/use-unity-context";
+import { UnityEventParameter } from "./types/unity-event-parameters";
 
 export { Unity, useUnityContext };
+export type { UnityEventParameter };
