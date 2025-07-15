03c40bd192c7623722dd2bffc77ab4f3601c7eda
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-15 16:24:13 +0200

    Add initialisationError state management to useUnityContext hook

--- module/source/hooks/use-unity-context.ts
+++ module/source/hooks/use-unity-context.ts
@@ -6,20 +6,23 @@ import { UnityProvider } from "../types/unity-provider";
 const useUnityContext = (unityConfig: UnityConfig): UnityContext => {
   const [loadingProgression, setLoadingProgression] = useState(0);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [initialisationError, setInitialisationError] = useState<Error>();
 
   const unityProvider = useRef<UnityProvider>({
     codeUrl: unityConfig.codeUrl,
     dataUrl: unityConfig.dataUrl,
     frameworkUrl: unityConfig.frameworkUrl,
     loaderUrl: unityConfig.loaderUrl,
-    setLoadingProgression: setLoadingProgression,
-    setIsLoaded: setIsLoaded,
+    setLoadingProgression,
+    setIsLoaded,
+    setInitialisationError,
   });
 
   return {
     unityProvider: unityProvider.current,
     loadingProgression,
     isLoaded,
+    initialisationError,
   };
 };
 
