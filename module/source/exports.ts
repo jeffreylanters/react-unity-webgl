ac1edf592db0219faca3765e4180f2359d38996e
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-16 15:24:28 +0200

    feat: export useUnityMetricsInfo from exports.ts

--- module/source/exports.ts
+++ module/source/exports.ts
@@ -1,6 +1,7 @@
 import { Unity } from "./components/unity";
 import { useUnityContext } from "./hooks/use-unity-context";
 import { UnityEventParameter } from "./types/unity-event-parameters";
+import { useUnityMetricsInfo } from "./hooks/use-unity-metrics-info";
 
-export { Unity, useUnityContext };
+export { Unity, useUnityContext, useUnityMetricsInfo };
 export type { UnityEventParameter };
