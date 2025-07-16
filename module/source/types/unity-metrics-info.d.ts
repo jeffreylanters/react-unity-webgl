b361396dd742668c6dfbc7ee52c21b99cb5a655c
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-16 15:24:38 +0200

    feat: make all properties of UnityMetricsInfo optional

--- module/source/types/unity-metrics-info.d.ts
+++ module/source/types/unity-metrics-info.d.ts
@@ -1,17 +1,17 @@
 type UnityMetricsInfo = {
-  assetLoadTime: number;
-  codeDownloadTime: number;
-  fps: number;
-  gameStartupTime: number;
-  movingAverageFps: number;
-  numJankedFrames: number;
-  pageLoadTime: number;
-  pageLoadTimeToFrame1: number;
-  totalJSHeapSize: number;
-  totalWASMHeapSize: number;
-  usedJSHeapSize: number;
-  usedWASMHeapSize: number;
-  webAssemblyStartupTime: number;
+  assetLoadTime?: number;
+  codeDownloadTime?: number;
+  fps?: number;
+  gameStartupTime?: number;
+  movingAverageFps?: number;
+  numJankedFrames?: number;
+  pageLoadTime?: number;
+  pageLoadTimeToFrame1?: number;
+  totalJSHeapSize?: number;
+  totalWASMHeapSize?: number;
+  usedJSHeapSize?: number;
+  usedWASMHeapSize?: number;
+  webAssemblyStartupTime?: number;
 };
 
 export type { UnityMetricsInfo };
