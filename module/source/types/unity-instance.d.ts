62b22feffd386d23df5a77906a7bbcbb2a676df5
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-16 11:11:51 +0200

    Refactor UnityInstance type declaration to include Quit method for improved functionality

--- module/source/types/unity-instance.d.ts
+++ module/source/types/unity-instance.d.ts
@@ -5,10 +5,13 @@ import { UnityModule } from "./unity-module";
 /**
  * Type declaration for the UnityInstance.
  */
-type UnityInstance = Pick<
-  UnityModule,
-  "SendMessage" | "SetFullscreen" | "Quit"
-> & {
+type UnityInstance = Pick<UnityModule, "SendMessage" | "SetFullscreen"> & {
+  /**
+   * Quits the Unity WebGL application and removes it from the memory.
+   * @returns a promise which resolves when the application did quit.
+   */
+  Quit(): Promise<void>;
+
   /**
    * The internal Unity Module.
    */
