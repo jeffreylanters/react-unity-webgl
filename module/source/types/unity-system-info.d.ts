2372def475212792f998a1c656e9ae6d352df231
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-19 13:30:16 +0200

    refactor: enhance UnitySystemInfo type definition with additional comments for clarity

--- module/source/types/unity-system-info.d.ts
+++ module/source/types/unity-system-info.d.ts
@@ -5,21 +5,52 @@
  * characteristics.
  */
 type UnitySystemInfo = {
+  /** Name of the browser (e.g., 'Chrome', 'Firefox'). */
   browser: string;
+
+  /** Version of the browser (e.g., '91.0.4472.124'). */
   browserVersion: string;
+
+  /** Name of the detected GPU (e.g., 'NVIDIA GeForce GTX 1050'). */
   gpu: string;
+
+  /** Indicates if cursor lock is supported. */
   hasCursorLock: boolean;
+
+  /** Indicates if fullscreen mode is supported. */
   hasFullscreen: boolean;
+
+  /** Indicates if threads are supported. */
   hasThreads: boolean;
+
+  /** Indicates if WebAssembly is supported. */
   hasWasm: boolean;
+
+  /** Indicates if WebAssembly threads are supported. */
   hasWasmThreads: boolean;
+
+  /** Indicates the supported WebGL version (1 or 2). */
   hasWebGL: 2 | 1;
+
+  /** Height of the browser window in pixels. */
   height: number;
+
+  /** Language of the browser (e.g., 'en-US'). */
   language: string;
+
+  /** Indicates if the device is mobile. */
   mobile: boolean;
+
+  /** Name of the operating system (e.g., 'Windows', 'macOS'). */
   os: string;
+
+  /** Version of the operating system (e.g., '10.15.7'). */
   osVersion: string;
+
+  /** User agent string of the browser. */
   userAgent: string;
+
+  /** Width of the browser window in pixels. */
   width: number;
 };
 
