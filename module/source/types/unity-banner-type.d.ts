facec05ca28c46c2919dd2530621e31362de299c
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-19 13:33:03 +0200

    refactor: enhance UnityBannerType definition with additional comments for clarity

--- module/source/types/unity-banner-type.d.ts
+++ module/source/types/unity-banner-type.d.ts
@@ -2,6 +2,16 @@
  * Banners can be used to display non-critical warnings and error messages from
  * the Unity Instance.
  */
-type UnityBannerType = "error" | "warning";
+type UnityBannerType =
+  /**
+   * Represents an error banner type. This banner type is used to display
+   * critical errors from the Unity Instance.
+   */
+  | "error"
+  /**
+   * Represents a warning banner type. This banner type is used to display
+   * non-critical warnings from the Unity Instance.
+   */
+  | "warning";
 
 export type { UnityBannerType };
