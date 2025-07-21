d9ce7727397ed3bf9dfa9861712953d68f1e6ba6
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-21 11:26:44 +0200

    refactor: expand exported types in exports.ts for improved type accessibility

--- module/source/exports.ts
+++ module/source/exports.ts
@@ -2,6 +2,18 @@ import { Unity } from "./components/unity";
 import { useUnityContext } from "./hooks/use-unity-context";
 import { UnityEventParameter } from "./types/unity-event-parameters";
 import { useUnityMetricsInfo } from "./hooks/use-unity-metrics-info";
+import { UnityBooleanLike } from "./types/unity-boolean-like";
+import { UnityCacheControlMode } from "./types/unity-cache-control-mode";
+import { UnityConfig } from "./types/unity-config";
+import { UnityInstance } from "./types/unity-instance";
+import { UnitySystemInfo } from "./types/unity-system-info";
 
 export { Unity, useUnityContext, useUnityMetricsInfo };
-export type { UnityEventParameter };
+export type {
+  UnityEventParameter,
+  UnityBooleanLike,
+  UnityCacheControlMode,
+  UnityConfig,
+  UnityInstance,
+  UnitySystemInfo,
+};
