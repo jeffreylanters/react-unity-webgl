558c9643e1422fc65ce2fc27b2cc90fcdbc732f5
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-21 10:52:36 +0200

    refactor: enhance documentation for UnityProvider methods for clarity

--- module/source/types/unity-provider.d.ts
+++ module/source/types/unity-provider.d.ts
@@ -20,22 +20,29 @@ type UnityProvider = Pick<
 > & {
   /**
    * Sets the Unity Context's loading progression.
+   * This is used to track the loading state of the Unity instance.
+   * @param loadingProgression The loading progression to set, typically a value between 0
    */
   readonly setLoadingProgression: (loadingProgression: number) => void;
 
   /**
    * Sets the Unity Context's loaded state.
+   * This is used to indicate whether the Unity instance has finished loading.
+   * @param isLoaded The loaded state to set, true if loaded, false otherwise.
    */
   readonly setIsLoaded: (isLoaded: boolean) => void;
 
   /**
    * Sets the Unity Context's initialisation error.
+   * This is used to handle errors that occur during the initialisation of the Unity instance.
+   * @param error The error to set, or undefined to clear the error.
    */
   readonly setInitialisationError: (error?: Error) => void;
 
   /**
    * Sets the Unity instance, used for internally managing the Unity instance
    * between the context and the UnityProvider.
+   * @param unityInstance The Unity instance to set, or null to clear it.
    */
   readonly setUnityInstance: (unityInstance: UnityInstance | null) => void;
 };
