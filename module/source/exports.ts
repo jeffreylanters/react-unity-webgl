0451325c01a7e4128555b39c351b632e91d813ca
Jeffrey Lanters <jeffreylanters@me.com>
2025-07-21 12:26:59 +0200

    fix: add UnityMessageParameter to exports for improved type accessibility

--- module/source/exports.ts
+++ module/source/exports.ts
@@ -1,6 +1,7 @@
 import { Unity } from "./components/unity";
 import { useUnityContext } from "./hooks/use-unity-context";
 import { UnityEventParameter } from "./types/unity-event-parameters";
+import { UnityMessageParameter } from "./types/unity-message-parameters";
 import { useUnityMetricsInfo } from "./hooks/use-unity-metrics-info";
 import { UnityBooleanLike } from "./types/unity-boolean-like";
 import { UnityCacheControlMode } from "./types/unity-cache-control-mode";
@@ -11,6 +12,7 @@ import { UnitySystemInfo } from "./types/unity-system-info";
 export { Unity, useUnityContext, useUnityMetricsInfo };
 export type {
   UnityEventParameter,
+  UnityMessageParameter,
   UnityBooleanLike,
   UnityCacheControlMode,
   UnityConfig,
