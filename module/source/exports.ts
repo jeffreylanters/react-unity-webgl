65d65a45b744e8a832b688d8af8be81c29881186
jeffreylanters <jeffreylanters@me.com>
2026-05-08 16:00:44 +0200

    fix: add UnityBannerType to exports and type exports

--- module/source/exports.ts
+++ module/source/exports.ts
@@ -5,6 +5,7 @@ import { UnityMessageParameter } from "./types/unity-message-parameters";
 import { useUnityMetricsInfo } from "./hooks/use-unity-metrics-info";
 import { UnityBooleanLike } from "./types/unity-boolean-like";
 import { UnityCacheControlMode } from "./types/unity-cache-control-mode";
+import { UnityBannerType } from "./types/unity-banner-type";
 import { UnityConfig } from "./types/unity-config";
 import { UnityInstance } from "./types/unity-instance";
 import { UnitySystemInfo } from "./types/unity-system-info";
@@ -13,6 +14,7 @@ export { Unity, useUnityContext, useUnityMetricsInfo };
 export type {
   UnityEventParameter,
   UnityMessageParameter,
+  UnityBannerType,
   UnityBooleanLike,
   UnityCacheControlMode,
   UnityConfig,
